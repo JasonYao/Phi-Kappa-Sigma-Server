b0a9146f870d62a311d20b4ab6207b5b611a7cbb
Jason Yao <jasony.edu@gmail.com>
2015-08-20 00:55:37 -0400

    Refactored out membership pages

---
+++ skullhouse/resources/views/public/membership/membership.blade.php
@@ -0,0 +1,44 @@
+@extends('public.carouselBase')
+
+@section('text')
+	<h1><span>MEMBERSHIP</span></h1>
+	<hr>
+
+	<p>
+	Skullhouse is keen to welcome into membership all men of good
+	character who share a common interest in our Core Values, so that
+	we can work together to further our objectives and to provide
+	mutual support to each other as members of the brotherhood.
+  </p>
+
+  <p>
+  We feel that it is important to stress potential applicants certain<br>
+  characteristics of Skullhouse.  We do this to help inquirers reach an<br>
+  informed decision about pursuing membership in our lifelong organization.
+  </p>
+
+  <p>
+  Firstly, we draw your attention to the Constitution and Acts of Phi<br>
+  Kappa Sigma International Fraternity, <a href="/PKS_Constitution.pdf"><span>available here in PDF</span></a>.<br>
+  Please read the document in full and, in particular, the following sections:
+  </p>
+  <ul>
+    <li><p><span>Article II</span> ("Membership")</p></li>
+    <li><p><span>Article X</span> ("Secrets and Initiation")</p></li>
+  </ul>
+
+  <p>
+  If you are interested in joining the Skullhouse, then please also take<br>
+  time to read through the <a href="/membership/faqs/"><span>Frequently Asked Questions (FAQs) section</span></a><br>
+  and <a href="/membership/expectations/"><span>Membership Expectations</span></a>.
+  </p>
+
+	<p>
+	<a href="/rush/"><span>Rush</span></a>
+	| <a href="/membership/recruitment/"><span>Recruitment</span></a>
+	| <a href="/membership/expectations/"><span>Membership Expectations</span></a>
+	<br>
+	 <a href="/membership/faqs/"><span>FAQs</span></a>
+	| <a href="/services/"><span>Member Services</span></a>
+	</p>
+@endsection
---
+++ skullhouse/resources/views/public/membership/recruitment.blade.php
@@ -0,0 +1,27 @@
+@extends('public.carouselBase')
+
+@section('text')
+	<h1><span>RECRUITMENT</span></h1>
+	<hr>
+
+	<p>
+	If you have read the Constitution and Acts of Phi Kappa Sigma<br>
+	International Fraternity and the FAQs and wish to express interest in<br>
+	becoming a Skull, please write to the Chi with a brief written<br>
+	explanation of what you feel attracts you to the Fraternity and how you<br>
+	would hope to involve yourself in its work.
+	</p>
+
+	<p>
+	If your initial response is approved, an Application Form (which<br>
+	includes a request for one reference) will be sent to you for<br>
+	completion and return to the Chi.
+	</p>
+
+	<p>
+	Application to the Fraternity is open to all men of good character, but<br>
+	please be aware that all applications are subject to several stages of<br>
+	review by the full brotherhood at New York University.<br>
+	Not all applications are successful.
+	</p>
+@endsection
