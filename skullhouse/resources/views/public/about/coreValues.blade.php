969d5f5ee0ce9f970e482dc08c38cb94344d76c3
Jason Yao <jasony.edu@gmail.com>
2015-08-20 00:54:13 -0400

    Refactored out all informational views

---
+++ skullhouse/resources/views/public/about/coreValues.blade.php
@@ -0,0 +1,56 @@
+@extends('public.carouselBase')
+
+@section('text')
+	<h1><span>CORE VALUES</span></h1>
+	<hr>
+
+	<p>
+	We commit to the value of <b><span>Trust</span></b>, the foundation of a<br>
+	relationship based on a belief in oneself as well as others, which is<br>
+	earned and strengthened through experience. The value of trust is<br>
+	found in our reliance on each other and enables an open comfort in<br>
+	interacting with fellow Skulls.
+	</p>
+
+	<p>
+	We commit to the value of <b><span>Honor</span></b>, staying true to a set of higher<br>
+	standards and morals in the face of adversity. Membership in Phi Kappa<br>
+	Sigma means a commitment to leading an honorable life.<br>
+	</p>
+
+	<p>
+ 	We commit to the value of <b><span>Respect</span></b>, which begins with yourself<br>
+	and the practice of the golden rule, “Do unto others as you would have<br>
+	them do unto you.” This statement is the acceptance of the ideals of<br>
+	others.  In order to move forward as a brotherhood, we must continue to<br>
+	consider each other’s points of view.<br>
+	</p>
+
+	<p>
+	We commit to the value of <b><span>Knowledge</span></b>, the acquisition of<br>
+	thought, theory, and principles following in the footsteps of our<br>
+	Founding Fathers in accordance with our oath of Brotherhood.  With<br>
+	this knowledge we will come closer to self-discovery, the improvement<br>
+	of society, and the translation of knowledge into power.<br>
+	</p>
+
+	<p>
+	We commit to the value of <b><span>Wisdom</span></b>, a virtue gained through<br>
+	experiences of self and others.  Wisdom is the application of knowledge<br>
+	and past experiences to make decisions regardless of the situation.<br>
+	Through sharing wisdom, a bond is formed between young and old,<br>
+	teacher and student; this bond is what brings us together.
+	</p>
+
+	<p>
+	We commit to the value of <b><span>Responsibility</span></b>, by being reliable in<br>
+	the fulfillment of the objectives of our Fraternity and our personal<br>
+	obligations, enduring the consequences of our actions.<br>
+	</p>
+
+	<p>
+	We commit to the value of <b><span>Integrity</span></b>, our inner foundation for<br>
+	holding true to our values of trust, honor, respect, knowledge, wisdom,<br>
+	and responsibility that guide us in living a life of virtue.<br>
+	</p>
+@endsection
