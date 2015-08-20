cbab4edaaeb9989001c1d9de27d6828427e8da3b
Jason Yao <jasony.edu@gmail.com>
2015-08-20 00:56:11 -0400

    Updated to fall 2015 rush information

---
+++ skullhouse/resources/views/public/rush/fall2015.blade.php
@@ -0,0 +1,78 @@
+<h1><span>FALL 2015 RUSH</span></h1>
+  <hr>
+
+  <p>
+    <b><a href="https://www.facebook.com/groups/RushSkullhouseNYU/"><span>
+      Applications are now open
+    </span></a></b><br>
+    Questions for the brothers?<br>
+    Call: <a href="tel:+14805401540"><span>(480) 540-1540</span></a><br>
+    Email:
+	<span id="obf"><script>document.getElementById("obf").innerHTML="<n uers=\"znvygb:alhfxhyyf+ehfu@tznvy.pbz?fhowrpg=Snyy 2015 Ehfu\" gnetrg=\"_oynax\">alhfxhyyf+ehfu@tznvy.pbz</n>".replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});document.body.appendChild(eo);</script>
+<noscript><span style="unicode-bidi:bidi-override;direction:rtl;">moc.liamg@hsur+slluksuyn</span></noscript></span>
+  </p>
+
+    <hr>
+
+<h3><span>Clubfest</span></h3>
+  <p>Thursday 9.3</p>
+  <p>
+    Start the week off right by meeting the brothers of Phi Kappa Sigma!
+  </p>
+  <p>TBA</p>
+
+  <h3><span>Meet the Greeks</span></h3>
+  <p>Wednesday 9.9</p>
+  <p>Kimmel E&L, 6:00pm</p>
+
+  <h3><span>Formal Recruitment 1</span></h3>
+  <p>Sunday 9.13</p>
+  <p>
+    A requirement from NYU's FSL. Optional, as long as you go to at least<br>
+    one FSL formal recruitment event. We recommend the 2nd formal recruitment
+  </p>
+  <p>Kimmel E&L, 1:00pm</p>
+
+  <h3><span>Billiards & Jazz</span></h3>
+  <p>Wednesday 9.16</p>
+  <p>
+    Come for a night of billiards, jazz, and fun with the Skulls! Do you<br>
+    have what it takes to beat the Skulls?
+  </p>
+  <p>Schwartz Plaza, 7pm</p>
+
+  <h3><span>Fratapult Night</span></h3>
+  <p>Monday 9.21</p>
+  <p>
+    Come together with brothers and new friends alike to<br>
+    build the best catapult Home Depot parts and duct tape<br>
+    can make.
+  </p>
+  <p>Schwartz Plaza, 7pm</p>
+
+  <h3><span>Soccer & Frisbee</span></h3>
+  <p>Thursday 9.24</p>
+  <p>
+  	Go toe to toe with some of the best football players at NYU,<br>
+  	and see how you stack up!
+  </p>
+  <p>Schwartz Plaza, 7pm</p>
+
+  <h3><span>Formal Recruitment 2</span></h3>
+  <p>Saturday 9.26</p>
+  <p>
+    The recommended day to go to formal recruitment,<br>
+    and a requirement for any rushee. We promise it won't<br>
+    be too bad, and after papers are filled in we'll make<br>
+    it up to those selected to get bids.
+  </p>
+  <p>TBA</p>
+
+  <h3><span>Invite-only Bid Dinner</span></h3>
+  <p>Saturday 9.26</p>
+  <p>
+    Those selected for bids will be invited to a dinner<br>
+    with the skulls. For those who accept,<br>
+    <span>welcome to the family</span>
+  </p>
+  <p>Schwartz Plaza, 7pm</p>
---
+++ skullhouse/resources/views/public/rush/rush.blade.php
@@ -0,0 +1,5 @@
+@extends('public.carouselBase')
+
+@section('text')
+	@include('public.rush.fall2015')
+@overwrite
