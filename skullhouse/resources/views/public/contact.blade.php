30798adc9699a638d18749ca23551b50fc48052d
Jason Yao <jasony.edu@gmail.com>
2015-08-20 00:55:07 -0400

    Added contact information TODO add separated out contact links

---
+++ skullhouse/resources/views/public/contact.blade.php
@@ -0,0 +1,13 @@
+@extends('public.carouselBase')
+
+@section('text')
+<h1><span>CONTACT US</span></h1>
+  <hr>
+  <p>
+	Questions for the brothers?<br>
+    Call: <a href="tel:+14805401540"><span>(480) 540-1540</span></a><br>
+    Email:
+	<span id="obf"><script>document.getElementById("obf").innerHTML="<n uers=\"znvygb:alhfxhyyf+ehfu@tznvy.pbz?fhowrpg=Snyy 2015 Ehfu\" gnetrg=\"_oynax\">alhfxhyyf+ehfu@tznvy.pbz</n>".replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});document.body.appendChild(eo);</script>
+<noscript><span style="unicode-bidi:bidi-override;direction:rtl;">moc.liamg@hsur+slluksuyn</span></noscript></span>
+  </p>
+@endsection
