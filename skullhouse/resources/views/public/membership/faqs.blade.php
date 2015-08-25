223b0a0ee7c73eca77e99266f47ff20df990d565
Jason Yao <jasony.edu@gmail.com>
2015-08-24 21:41:36 -0400

    Refactored faq styling to be more consistent with site

--- skullhouse/resources/views/public/membership/faqs.blade.php
+++ skullhouse/resources/views/public/membership/faqs.blade.php
@@ -1,63 +1,68 @@
 @extends('public.carouselBase')
 
 @section('text')
-    <h1>FAQs</h1>
-    <p>
+    <h1><span>FAQs</span></h1>
+	<hr>
+
+	<p>
       If you are interested in joining Skullhouse, then please also take time
       to read through the following answers to frequently asked questions.
     </p>
     <p>
       This list is not intended to be comprehensive, but answers the most
       commonly asked questions. Should you have any further questions not
-      listed here, please feel free to contact the Chi (Recruitment Chair) by
-      clicking Recruitment above.
+      listed here, please feel free to <a href="/contact/"><span>contact us</span></a>.
     </p>
+	<hr>
 
-    <!-- Question 1 -->
-    <div class="alert alert-success" role="alert">
-    <strong>Question:</strong> What does the Skullhouse expect of someone looking to join?
-    </div>
-
-    <div class="alert alert-info" role="alert">
+	<!-- Question 1 -->
+	<p>
+    <span><strong>Question:</strong> What does the Skullhouse expect of someone looking to join?</span>
+	</p>
+	
+    <p>
     <strong>Answer:</strong> In Phi Kappa Sigma, we lay particular emphasis on the personal
       commitment of members to the Fraternity’s Core Values and code of
       conduct, and expect members to set an example in on campus and in their
       communities by their conduct.
-    </div>
+    </p>
+    <hr>
 
     <!-- Question 2 -->
-    <div class="alert alert-success" role="alert">
-    <strong>Question:</strong> How much time and effort should I expect to give the Skullhouse?
-    </div>
+   <p>
+    <span><strong>Question:</strong> How much time and effort should I expect to give the Skullhouse?</span>
+    </p>
 
-    <div class="alert alert-info" role="alert">
+    <p>
     <strong>Answer:</strong> In the tradition of the fraternal and secret society that Skullhouse
-      originated as, oursis very much a working Fraternity. As such, full
+      originated as, ours is very much a working Fraternity. As such, full
       members are expected to attend weekly meetings and to support -
       according to their financial means and through their active
       participation - the Fraternity’s philanthropic, service, cultural,
       social, and traditional objectives. Ours is a lifelong brotherhood, and
       membership should not be taken lightly.
-    </div>
+    </p>
+    <hr>
 
     <!-- Question 3 -->
-    <div class="alert alert-success" role="alert">
-    <strong>Question:</strong> What status does the Skullhouse have nationally?
-    </div>
+   <p>
+    <span><strong>Question:</strong> What status does the Skullhouse have nationally?</span>
+    </p>
 
-    <div class="alert alert-info" role="alert">
+    <p>
     <strong>Answer:</strong> Phi Kappa Sigma is a founding member of the North-American
       Interfraternity Conference (NIC). We therefore have a responsibility to
       ensure that we maintain the standing and reputation of the Fraternity.
-    </div>
+    </p>
+    <hr>
 
-    <!-- Question 4 -->
-    <div class="alert alert-success" role="alert">
-    <strong>Question:</strong> These are high expectations – what if I find it difficult to meet
-      them?
-    </div>
+   <!-- Question 4 -->
+   <p>
+    <span><strong>Question:</strong> These are high expectations – what if I find it difficult to meet
+      them?</span>
+    </p>
 
-    <div class="alert alert-info" role="alert">
+    <p>
     <strong>Answer:</strong> We think it is right to aspire to high ideals in the Fraternity, but
       we recognize that everyone needs help and encouragement to achieve
       them. Experienced Skulls act as mentors to successful applicants and
@@ -65,77 +70,84 @@
       initiation and investiture. However, support does not cease there as
       all Skulls are encouraged to support and help one another in such a
       manner as to foster a good team spirit.
-    </div>
+    </p>
+    <hr>
 
     <!-- Question 5 -->
-    <div class="alert alert-success" role="alert">
-    <strong>Question:</strong> Does the Skullhouse accept applications from all undergraduate
-      programs?
-    </div>
+   <p>
+    <span><strong>Question:</strong> Does the Skullhouse accept applications from all undergraduate
+      programs?</span>
+    </p>
 
-    <div class="alert alert-info" role="alert">
+    <p>
     <strong>Answer:</strong> Yes – ours is a university-wide organization. We admit
       undergraduates from all majors, programs, and schools of New York
       University.
-    </div>
+    </p>
+    <hr>
 
     <!-- Question 6 -->
-    <div class="alert alert-success" role="alert">
-    <strong>Question:</strong> I plan on studying away for one or more semesters – can I still
-      apply?
-    </div>
+   <p>
+    <span><strong>Question:</strong> I plan on studying away for one or more semesters – can I still
+      apply?</span>
+    </p>
 
-    <div class="alert alert-info" role="alert">
+    <p>
     <strong>Answer:</strong> Yes. Many Skulls have studied abroad, and we expect future Skulls to
       continue in this tradition.
-    </div>
+    </p>
+    <hr>
 
     <!-- Question 7 -->
-    <div class="alert alert-success" role="alert">
-    <strong>Question:</strong> I am a transfer student, an upperclassman, or a veteran – can I
-      still apply?
-    </div>
+   <p>
+    <span><strong>Question:</strong> I am a transfer student, an upperclassman, or a veteran – can I
+      still apply?</span>
+    </p>
 
-    <div class="alert alert-info" role="alert">
+    <p>
     <strong>Answer:</strong> Yes. If you are enrolled full time as an undergraduate student at
       New York University, you are eligible for membership.
-    </div>
+    </p>
+    <hr>
 
     <!-- Question 8 -->
-    <div class="alert alert-success" role="alert">
-    <strong>Question:</strong> What do membership dues pay for?
-    </div>
+   <p>
+    <span><strong>Question:</strong> What do membership dues pay for?</span>
+    </p>
 
-    <div class="alert alert-info" role="alert">
+    <p>
     <strong>Answer:</strong> Membership dues pay for the Skullhouse’s operations and expenditures
       throughout the year. This includes insurance, apparel, and social,
       philanthropic, and service events.
-    </div>
+    </p>
+    <hr>
 
     <!-- Question 9 -->
-    <div class="alert alert-success" role="alert">
-    <strong>Question:</strong> What is rush week?
-    </div>
+   <p>
+    <span><strong>Question:</strong> What is rush week?</span>
+    </p>
 
-    <div class="alert alert-info" role="alert">
-    <strong>Answer:</strong> Rush Week, or simply “rush,” is a weeklong period every semester
+    <p>
+    <strong>Answer:</strong> Rush Week, or simply “rush,” is a two week period every semester
       when each fraternity hosts events in order to meet potential new
       members (or “rushees”). The idea is for rushees to meet and mingle with
       the fraternities in order to find the group of men that suits them
       best, and then narrow their rushing efforts on the specific house they
       have the strongest affinity for.
-    </div>
+    </p>
+    <hr>
 
     <!-- Question 10 -->
-    <div class="alert alert-success" role="alert">
-    <strong>Question:</strong> What is a bid?
-    </div>
+   <p>
+    <span><strong>Question:</strong> What is a bid?</span>
+    </p>
 
-    <div class="alert alert-info" role="alert">
+    <p>
     <strong>Answer:</strong> A bid is a formal invitation to join a fraternity. Some
       fraternities, like Skullhouse, present these as cards with the
       potential new member’s name or initials printed on it, and some present
       their bids via an in person conversation, email, or even by text
       message.
-    </div>
+    </p>
+
 @endsection
