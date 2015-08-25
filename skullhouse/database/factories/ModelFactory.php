e7fee8246c29ce391d357df9d8451e03350551a4
Jason Yao <jasony.edu@gmail.com>
2015-08-25 04:53:23 -0400

    Changed name fields for model factory

--- skullhouse/database/factories/ModelFactory.php
+++ skullhouse/database/factories/ModelFactory.php
@@ -13,7 +13,8 @@
 
 $factory->define(App\User::class, function ($faker) {
     return [
-        'name' => $faker->name,
+        'fistName' => $faker->firstName,
+		'lastName' => $faker->lastName,
         'email' => $faker->email,
         'password' => str_random(10),
         'remember_token' => str_random(10),
