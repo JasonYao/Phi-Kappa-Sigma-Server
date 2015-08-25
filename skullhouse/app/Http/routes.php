635fb97d27360bce9589e0ccce646791c18a535b
Jason Yao <jasony.edu@gmail.com>
2015-08-24 23:15:11 -0400

    Added dashboard route

--- skullhouse/app/Http/routes.php
+++ skullhouse/app/Http/routes.php
@@ -53,12 +53,14 @@
 	// Login & Registration
 	//Route::controller('/', 'Auth\AuthController');
 
-// Authentication routes...
-Route::get('login', 'Auth\AuthController@getLogin');
-Route::post('login', 'Auth\AuthController@postLogin');
-Route::get('logout', 'Auth\AuthController@getLogout');
+	// Authentication routes
+		Route::get('login', 'Auth\AuthController@getLogin');
+		Route::post('login', 'Auth\AuthController@postLogin');
+		Route::get('logout', 'Auth\AuthController@getLogout');
 
-// Registration routes...
-Route::get('register', 'Auth\AuthController@getRegister');
-Route::post('register', 'Auth\AuthController@postRegister');
+	// Registration routes
+		Route::get('register', 'Auth\AuthController@getRegister');
+		Route::post('register', 'Auth\AuthController@postRegister');
 
+	// Authenticed routes
+		Route::get('dashboard', 'ValidatedPagesController@getDashboard');
