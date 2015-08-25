4d949149988942313a7e08f197513d82c8618eb9
Jason Yao <jasony.edu@gmail.com>
2015-08-25 05:09:27 -0400

    Renamed for easier tracking

--- skullhouse/database/migrations/2015_01_01_000000_create_users_table.php
+++ skullhouse/database/migrations/2015_01_01_000000_create_users_table.php
@@ -14,9 +14,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
+            $table->string('firstName', 30);
+			$table->string('lastName', 30);
+            $table->string('email', 50)->unique();
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
