5b41613c7f348467888bef26b3dbfa60077af922
Jason Yao <jasony.edu@gmail.com>
2015-08-24 21:37:28 -0400

    Added social icons to pages

---
+++ skullhouse/resources/views/includes/social.blade.php
@@ -0,0 +1,9 @@
+<!-- Social icons -->
+<div class="social">
+    <ul class="social-icons">
+        <li><a href="https://www.facebook.com/NYUSkulls" data-original-title="Facebook" class="social_facebook"></a></li>
+        <li><a href="https://twitter.com/nyuskulls" data-original-title="Twitter" class="social_twitter"></a></li>
+        <li><a href="https://www.linkedin.com/groups/Phi-Kappa-Sigma-International-Fraternity-2392/about" data-original-title="Linkedin" class="social_linkedin"></a></li>
+    </ul>
+</div>
+
