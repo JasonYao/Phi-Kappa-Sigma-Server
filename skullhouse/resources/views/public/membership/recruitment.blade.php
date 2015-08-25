88ac11c1ab704d51b8b833891ca2a953a9f357ec
Jason Yao <jasony.edu@gmail.com>
2015-08-24 21:42:15 -0400

    Removed info page and combined it with the recruitment page

--- skullhouse/resources/views/public/membership/recruitment.blade.php
+++ skullhouse/resources/views/public/membership/recruitment.blade.php
@@ -4,24 +4,21 @@
 	<h1><span>RECRUITMENT</span></h1>
 	<hr>
 
-	<p>
-	If you have read the Constitution and Acts of Phi Kappa Sigma<br>
-	International Fraternity and the FAQs and wish to express interest in<br>
-	becoming a Skull, please write to the Chi with a brief written<br>
-	explanation of what you feel attracts you to the Fraternity and how you<br>
-	would hope to involve yourself in its work.
-	</p>
+<!-- Recruitment text -->
+<p>
+  Skullhouse is keen to welcome into membership all men of good character who share a common interest in our core values, so that we can work together to further our objectives and to provide mutual support to each other as members of the brotherhood.
+</p>
 
-	<p>
-	If your initial response is approved, an Application Form (which<br>
-	includes a request for one reference) will be sent to you for<br>
-	completion and return to the Chi.
-	</p>
+<p>
+  If you have read the FAQs and Membership Expectations and express an interest in becoming a Skull, please fill out this <a href="https://docs.google.com/forms/d/1GbAJMpDxUrMCDHORlMfrL7E7F-m7-hg-7tTIEExijUA/viewform"><span>form</span></a>.
+</p>
 
-	<p>
-	Application to the Fraternity is open to all men of good character, but<br>
-	please be aware that all applications are subject to several stages of<br>
-	review by the full brotherhood at New York University.<br>
-	Not all applications are successful.
-	</p>
+<p>
+  Application to the Fraternity is open to all men of good character, but please be aware that all applications are subject to several stages of review by the full brotherhood at New York University.
+</p>
+
+<p>Not all applications are successful.</p>
+
+<p><a href="/membership/faq/"><span>FAQs</span></a> | <a href="/membership/expectations/"><span>Membership Expectations</span></a></p>
+
 @endsection
