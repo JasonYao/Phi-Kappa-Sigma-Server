ac6cf2522ae09429d220cdbfe62a00e991fcb526
Jason Yao <jasony.edu@gmail.com>
2015-08-26 18:38:52 -0400

    Added error handling TODO fix registration errorbag

--- skullhouse/resources/views/includes/messages.blade.php
+++ skullhouse/resources/views/includes/messages.blade.php
@@ -2,6 +2,18 @@
 @if (Session::has('flashMessage'))
 	<div class="alert alert-success">
 		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-		{{ Session::get('flashMessage') }}
+		{!! Session::get('flashMessage') !!}
 	</div>
 @endif
+
+@if (count($errors) > 0)
+<div class="alert alert-danger">
+	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+	<ul>
+		@foreach ($errors->all() as $error)
+			<li>{!! $error !!}</li>
+		@endforeach
+	</ul>
+</div>
+@endif
+
