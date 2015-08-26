15870b39bd3ea9bc1b5304b2247a70b77f2d173b
Jason Yao <jasony.edu@gmail.com>
2015-08-26 18:14:30 -0400

    Added facebook meta tags

--- skullhouse/resources/views/includes/partials/meta/global.blade.php
+++ skullhouse/resources/views/includes/partials/meta/global.blade.php
@@ -4,6 +4,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Opengraph Metadata tags: http://ogp.me/ -->
+    <meta property="og:title" content="NYU Skullhouse | Phi Kappa Sigma Fraternity | Delta Phi Chapter | New York University" />
+    <meta property="og:description" content="Skullhouse is New York University’s premier society for men who seek intellectual stimulation, lifelong friendships, and a network of passionate individuals who share common values." />
+	<meta property="og:url" content="@if(Request::url() === 'https://www.skullhouse.nyc'){{Request::url()}}@else{{Request::url() . '/'}}@endif" />
+
+    <meta property="og:image" content="https://www.skullhouse.nyc/assets/img/social/fbLink.png" />
+    <meta property="og:image:secure_url" content="https://www.skullhouse.nyc/assets/img/social/fbLink.png" />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:image:width" content="669" />
+    <meta property="og:image:height" content="842" />
+
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="Skullhouse is New York University’s premier society for men who seek intellectual stimulation, lifelong friendships, and a network of passionate individuals who share common values.">
     <meta name="author" content="Phi Kappa Sigma Delta Phi chapter">
