baf05f935a899ff19c3111298aa12abaaf2dae5f
Jason Yao <jasony.edu@gmail.com>
2015-08-27 06:47:16 -0400

    Added other attributes of a user

--- skullhouse/database/migrations/2015_01_01_000000_create_users_table.php
+++ skullhouse/database/migrations/2015_01_01_000000_create_users_table.php
@@ -12,16 +12,32 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table)
+		{
             $table->increments('id');
+
+			// Basic account information
             $table->string('firstName', 30);
+			$table->string('middleInitial')->nullable();
 			$table->string('lastName', 30);
             $table->string('email', 50)->unique();
             $table->string('password');
+
+			// Fraternity-specific information
+			$table->text('description')->nullable();
+			$table->string('picture')->nullable();
+			$table->string('initiationClass')->nullable();
+			$table->string('degree')->nullable();
+			$table->string('school')->nullable();
+			$table->string('honours')->nullable();
+
+			// Technical information
+			$table->string('confirmationCode')->nullable(); // If code is null, account is confirmed
+			$table->string('obfuscationCode'); // Don't null this shit, since image folders depend on this
             $table->rememberToken();
             $table->timestamps();
         });
-    }
+    } // End of the users table
 
     /**
      * Reverse the migrations.
