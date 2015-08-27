a89dec19913b2067ffefceb573942cc38cf01874
Jason Yao <jasony.edu@gmail.com>
2015-08-27 06:47:36 -0400

    Added brittons welcome message

--- skullhouse/resources/views/private/dashboard.blade.php
+++ skullhouse/resources/views/private/dashboard.blade.php
@@ -9,6 +9,11 @@
 	<h1><span>{{ Auth::user()->firstName . "'s" }} Dashboard</span></h1>
 	<hr>
 
+	<p><span>Once a Phi Kap, Always a Phi Kap</span></p>
+	<p>
+		We'd love to keep everyone up to date on your most recent success. The links below will allow you to view or update your publicly available biography on the Brothers page.
+	</p>
+
 	<p>
 		View your profile <a href="/profile/"><span>here</span></a>.
 	</p>
@@ -18,6 +23,6 @@
 	</p>
 
 	<p>
-		Update your account password <a href="/password/email/"><span>here</span></a>
+		Reset your account password <a href="/password/email/"><span>here</span></a>
 	</p>
 @endsection
