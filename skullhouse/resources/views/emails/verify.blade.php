f185ec2e7b628f0d711be037f8d4294b7d01e4c0
Jason Yao <jasony.edu@gmail.com>
2015-08-31 00:46:37 -0400

    Added email confirmation

---
+++ skullhouse/resources/views/emails/verify.blade.php
@@ -0,0 +1,22 @@
+@extends('public.carouselBase')
+
+@section('css')
+	@include('includes.partials.css.forms')
+@overwrite
+
+@section('text')
+
+	<h1><span>Email confirmation</span></h1>
+	<hr>
+
+	<p>
+		Click here to confirm your email account and gain access to your profile: {{ URL::to('register/verify/' . $user->confirmationCode . '/') }}
+	</p>
+
+	<!-- Registration in case of no account -->
+	<hr>
+	<p>
+		If you don't have an account, sign up for one <a href="/register/"><span>here</span></a>. After you register, an admin will approve/deny your account if you are from Delta Phi.
+	</p>
+	<hr>
+@endsection
