f6ab62d741d989920f376fde95c8d0471ad9ffd6
Jason Yao <jasony.edu@gmail.com>
2015-09-13 14:23:25 -0400

    Added pdf version of the letter from the president

--- skullhouse/resources/views/public/home.blade.php
+++ skullhouse/resources/views/public/home.blade.php
@@ -3,34 +3,9 @@
 @section('text')
 	<h1><span>LETTER FROM THE PRESIDENT</span></h1>
 	<hr>
-	September 10th, 2015<br>
 
-	<br>
-	<p>
-		Dear <span>Prospective Skull</span>,
-	</p>
-	<p>
-		It is an honor that you are considering joining the Delta Phi chapter of the Phi Kappa Sigma fraternity here at NYU.
-		Phi Kappa Sigma is a social fraternity that is looking for intellectual, outgoing, and adventurous students to join its ranks.
-		Skulls are very involved on campus. Athletics are very important to the brothers-
-		for example, <a href="/brothers/JasonYao/">Jason</a> and <a href="/brothers/ColinSherman/">Colin</a> compete on NYU’s fencing team while <a href="/brothers/WilliamGockel-Figge/">Will</a> competes on the wrestling team.
-		In the past year, NYU has awarded our chapter for the <span>highest GPA of any social fraternity</span>, most improved chapter, best risk management, and <span>excellence in values</span>.
-		The brothers here have had an excellent summer, from working on campaign finance in DC and travelling around Europe, to interning at Microsoft and PricewaterhouseCoopers - we’re all rested and ready for a great school year.
-	</p>
-	<p>
-		We are excited for recruitment this semester and look forward to meeting you.
-		The recruitment process begins with you <a href="https://docs.google.com/a/nyu.edu/forms/d/16WKx2eI0_wSfFgQGKUr4UOI7UMipO-51YydFG3-tv3s/viewform">registering for recruitment</a> 
-		with the NYU Interfraternity Council. To learn more about upcoming recruiting <a href="/events/">events</a> “Billiards and Jazz,” “Fratapult,” and “Soccer and Frisbee,” 
-		please join our <a href="https://www.facebook.com/groups/RushSkullhouseNYU/">Rush Facebook group</a>.
-	</p>
-	<p>
-		To all the freshmen and transfer students on campus, congratulations on enrolling at NYU. We are excited to have you on campus and are looking forward to helping to make NYU and New York City your new home.<br>
-		Here’s to the start of a great year.
-	</p>
 
 	<p>
-		Sincerely,<br>
-		<a href="/brothers/RobertGardner/">Bob Gardner</a><br>
-		President, Phi Kappa Sigma
+		<a href="/letter.pdf">Please click here for a letter from our president</a>
 	</p>
 @endsection
