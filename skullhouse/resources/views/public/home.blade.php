3788c76dd259c4d8522a700352d2d19eb7088693
Jason Yao <jasony.edu@gmail.com>
2015-09-14 00:49:40 -0400

    Added original text to home  page

--- skullhouse/resources/views/public/home.blade.php
+++ skullhouse/resources/views/public/home.blade.php
@@ -4,8 +4,52 @@
 	<h1><span>LETTER FROM THE PRESIDENT</span></h1>
 	<hr>
 
+	<p>Dear Prospective Skull,</p>
 
 	<p>
-		<a href="/letter.pdf">Please click here for a letter from our president</a>
+		It is an honor that you are considering joining the Delta Phi<br>
+		Chapter of the Phi Kappa Sigma Fraternity at New York University.<br>
+		Skullhouse is always looking for socially adventurous and<br>
+		intellectually curious members, and we look forward to making<br>
+		your acquaintance!
+	</p>
+
+	<p>
+		Despite the academic rigors of one of the nation’s most outstanding universities, the brothers of 
+		Phi Kappa Sigma have not only endured, but have thrived, finding success in their coursework 
+		and extracurricular activities. Like New York City itself, we are truly a diverse and cosmopolitan 
+		band of brothers — it’s almost a guarantee that someone in the Chapter shares your interest.  
+	</p>
+
+	<p>
+		The <a href="/brothers/">brothers</a> of Skullhouse are varsity sportsmen, first chair violinists, leaders in the university’s 
+		developer and technology communities, filmmakers, volunteer museum guides, avid sailors, and 
+		public policy case competitors. This summer, brothers secured internships developing apps for 
+		Microsoft, investigating campaign finance in DC, and consulting businesses for Pricewaterhouse 
+		Coopers. Our members come from as far away as Nanjing and as close as the Bronx. 
+	</p>
+
+	<p>
+		We are excited for recruitment this semester and invite you to be a part of the most tight-knit 
+		brotherhood on campus. To take the first step, please 
+		register for recruitment
+		with the Interfraternity Council. To learn more about Skullhouse’s “Billiards and Jazz,” “Fratapult,” and 
+		“Soccer and Frisbee” <a href="/events/">recruitment events</a>, 
+		join our <a href="https://www.facebook.com/groups/RushSkullhouseNYU/">Facebook group</a>. And of course, please browse 
+		this website — it’s a treasure trove of information! 
+	</p>
+
+	<p>
+		Welcome (back) to campus! Here’s to the start of a great year. 
+	</p>
+
+	<p>
+		Sincerely,<br>
+		Bob Gardner<br>
+		President, Phi Kappa Sigma
+	</p>
+
+	<p>
+		<a href="/letter.pdf">Please click here for a pdf version of the President's letter</a>
 	</p>
 @endsection
