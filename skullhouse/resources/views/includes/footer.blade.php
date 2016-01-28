e685c02cf31a05201298ed0864b55ebb51653d93
Jason Yao <jasony.edu@gmail.com>
2016-01-28 13:47:44 -0500

    Made footer rights year automatically update

--- skullhouse/resources/views/includes/footer.blade.php
+++ skullhouse/resources/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- Footer start -->
 	<div id="footer">
-		<p>All rights reserved. &#169; 2015 Phi Kappa Sigma - Delta Phi Chapter</p>
+		<p>All rights reserved. &#169; <?php echo date("Y"); ?> Phi Kappa Sigma - Delta Phi Chapter</p>
 	</div>
 <!-- Footer end -->
